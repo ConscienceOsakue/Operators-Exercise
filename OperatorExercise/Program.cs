1046c49308849dc55b1a37d55622fff82bd99b42
conscience osakue <consciencenosa@gmail.com>
2023-11-14 17:16:18 -0600

    Exercise1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //For division create two integer variable called a and b
+
+            var a = 17;
+            var b = 4;
+            var sum = a + b;
+            var subtr = a - b;
+            var multi = a * b;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} + {b} = {sum}, {a} - {b} = {subtr}, {a} * {b} = {multi}, {a} / {b} is {quotient} remainder {remainder}" );
         }
     }
 }
